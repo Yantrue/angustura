6fc6d55d9c9d2172f0530c8e7169621326554bcb
Yantrue <riyansanjaya703@gmail.com>
2026-04-23 20:44:11 +0700

    Update form.js

--- modules/form.js
+++ modules/form.js
@@ -1,1 +1,26 @@
+import { createInput, createTextarea } from './input.js';
+import { saveData } from './storage.js';
 
+export function renderForm(root) {
+  const container = document.createElement('div');
+  container.className = 'container';
+
+  const name = createInput('Nama');
+  const email = createInput('Email');
+  const pesan = createTextarea('Pesan');
+
+  const btn = document.createElement('button');
+  btn.textContent = 'Kirim';
+
+  btn.onclick = () => {
+    saveData({
+      nama: name.value,
+      email: email.value,
+      pesan: pesan.value
+    });
+    alert('Tersimpan');
+  };
+
+  container.append(name, email, pesan, btn);
+  root.appendChild(container);
+}
