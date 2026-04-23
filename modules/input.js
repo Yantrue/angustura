dd91be1d8d524d5f65878ab0032f6ecd24932019
Yantrue <riyansanjaya703@gmail.com>
2026-04-23 20:44:24 +0700

    Update input.js

--- modules/input.js
+++ modules/input.js
@@ -1,1 +1,11 @@
+export function createInput(placeholder) {
+  const el = document.createElement('input');
+  el.placeholder = placeholder;
+  return el;
+}
 
+export function createTextarea(placeholder) {
+  const el = document.createElement('textarea');
+  el.placeholder = placeholder;
+  return el;
+}
